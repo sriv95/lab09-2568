afa2a14f346bb98de6a27e7a79e97fa3c7f67d90
Parzival <srivichai95@gmail.com>
2025-08-06 00:24:50 +0700

    Implement counter functionality in CounterPage component

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
+
 export default function CounterPage() {
+  const [counter, setCounter] = useState(0)
   return (
     <div className="container text-center">
       <h2>Counter Page</h2>
       {/* Result Text */}
-      <p>Counter:</p>
-      <button>Increase</button>
+      <p>Counter:{counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>Increase</button>
     </div>
   );
 }
